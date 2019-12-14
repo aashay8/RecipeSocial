41ea2b9636343de73dd0a81d237646958d88ee19
aashay8 <aashay8@gmail.com>
2019-12-14 15:07:44 +0530

    Added CORS

--- docs/api_project.js
+++ docs/api_project.js
@@ -7,7 +7,7 @@ define({
   "defaultVersion": "0.0.0",
   "generator": {
     "name": "apidoc",
-    "time": "2019-12-14T08:01:53.195Z",
+    "time": "2019-12-14T08:41:52.029Z",
     "url": "http://apidocjs.com",
     "version": "0.19.0"
   }
